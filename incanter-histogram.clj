50e47a9490e086612dcac7f9324ac73352d2245b
Justin Burris <justinbburris@gmail.com>
2012-05-07 22:24:14 -0400

    fixing styles

--- incanter-histogram.clj
+++ incanter-histogram.clj
@@ -1,29 +1,29 @@
 ;;;; Imports data from a return separated sample_data file & displays it using a histogram
 (use '(incanter core charts datasets stats io))
 
-; Initial dataset to compare against
+;; Initial dataset to compare against
 (def data
   (read-dataset "/Users/jburris/Development/Zeevex/incanter/purchase-time-to-completions.csv"
                 :delim \return
                 :header true))
 
-; Displays a dataset with a 'Seconds' column using histogram
+;; Displays a dataset with a 'Seconds' column using histogram
 (defn show-histogram [dataset]
   (view (histogram
        :Seconds
        :data dataset
        :y-label "Count")))
 
-; Specifies the range to work with for the data
+;; Specifies the range to work with for the data
 (defn range-data [dataset gt lt]
   (with-data dataset (->
                       ($where
                        {:Seconds {:$lt lt :$gt gt }}))))
 
-; Ranges we're looking to gather histograms from
+;; Ranges we're looking to gather histograms from
 (def ranges [[-100 0] [0 20] [20 100] [100 1000] [1000 500000] [500000 3000000] ] )
 
-; map over all of the range options, displaying a histogram for each
+;; map over all of the range options, displaying a histogram for each
 (map
  (fn [[gt lt]] (show-histogram (range-data data gt lt)))
  ranges)
